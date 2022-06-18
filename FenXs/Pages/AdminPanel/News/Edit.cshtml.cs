f0585dd6d5269e18efcecc145b1f6d5f38329216
UltiPro <106282113+UltiPro@users.noreply.github.com>
2022-06-18 02:30:29 +0200

    Edit News, Corrections
    +Add Edit and correct FenXs.css

---
+++ FenXs/Pages/AdminPanel/News/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using PageModels.AdminPageModel;
+using DAL.FenXsNewsDAL;
+using Models.NewsModel;
+
+namespace FenXs.Pages;
+
+public class NewsEditModel : AdminPageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public int id { get; set; }
+    private FenXsNewsDAL FND;
+    [BindProperty]
+    public News N { get; set; }
+    public bool ErrorBox;
+    public string Info;
+    public NewsEditModel(IConfiguration _configuration)
+    {
+        FND = new FenXsNewsDAL(_configuration);
+    }
+    public IActionResult OnPost()
+    {
+        ErrorBox = false;
+        if (ModelState.IsValid)
+        {
+            N.id = id;
+            if (FND.UpdateNews(N) == 0) return RedirectToPage("../News");
+            else Info = "Currently, the server cannot fulfill the request.";
+        }
+        ErrorBox = true;
+        return Page();
+    }
+}
