43d9e58ab4b448315493b996b264fe88d240360e
UltiPro <106282113+UltiPro@users.noreply.github.com>
2022-06-18 03:02:51 +0200

    Sumary of day work
    Need to correct

---
+++ FenXs/Pages/AdminPanel/News/Add.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using PageModels.AdminPageModel;
+using DAL.FenXsNewsDAL;
+using Models.NewsModel;
+
+namespace FenXs.Pages;
+
+public class NewsAddModel : AdminPageModel
+{
+    private FenXsNewsDAL FND;
+    [BindProperty]
+    public News N { get; set; }
+    public bool ErrorBox;
+    public string Info;
+    public NewsAddModel(IConfiguration _configuration)
+    {
+        FND = new FenXsNewsDAL(_configuration);
+    }
+    public IActionResult OnPost()
+    {
+        ErrorBox = false;
+        if (ModelState.IsValid)
+        {
+            if (FND.AddNews(N) == 0) return RedirectToPage("../News");
+            else Info = "Currently, the server cannot fulfill the request.";
+        }
+        ErrorBox = true;
+        return Page();
+    }
+}
--- FenXs/Pages/Settings/Index.cshtml.cs
+++ FenXs/Pages/Settings/Index.cshtml.cs
@@ -1,8 +1,13 @@
 using PageModels.LoggedPageModel;
+using DAL.FenXsAccountDAL;
 
 namespace FenXs.Pages;
 
 public class SettingsIndexModel : LoggedPageModel
 {
-
+    private FenXsAccountDAL FAD;
+    public SettingsIndexModel(IConfiguration _configuration)
+    {
+        FAD = new FenXsAccountDAL(_configuration);
+    }
 }
