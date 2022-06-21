1bf45c73f4b99a3eae383e87cfa1818700415c5e
UltiPro <106282113+UltiPro@users.noreply.github.com>
2022-06-21 19:58:01 +0200

    Update

---
+++ FenXs/Pages/AdminPanel/News.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using PageModels.AdminPageModel;
+using Models.NewsModel;
+using DAL.FenXsNewsDAL;
+
+namespace FenXs.Pages;
+
+public class NewsModel : AdminPageModel
+{
+    private FenXsNewsDAL fenXsNewsDAL;
+    public List<News> listOfNews;
+    public NewsModel(IConfiguration configuration)
+    {
+        fenXsNewsDAL = new FenXsNewsDAL(configuration);
+        listOfNews = fenXsNewsDAL.GetNews(false);
+    }
+}
