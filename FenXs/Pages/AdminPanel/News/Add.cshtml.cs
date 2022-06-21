4208f2073a7956ba386e2f9bd6d28f1d60a5fdf2
UltiPro <106282113+UltiPro@users.noreply.github.com>
2022-06-21 19:59:29 +0200

    Update

---
+++ FenXs/Pages/AdminPanel/News/Add.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using PageModels.AdminPageModel;
+using DAL.FenXsNewsDAL;
+using Models.NewsModel;
+
+namespace FenXs.Pages;
+
+public class NewsAddModel : AdminPageModel
+{
+    private FenXsNewsDAL fenXsNewsDAL;
+    [BindProperty]
+    public News news { get; set; }
+    public bool dangerBox;
+    public string info;
+    public NewsAddModel(IConfiguration configuration)
+    {
+        fenXsNewsDAL = new FenXsNewsDAL(configuration);
+    }
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            if (fenXsNewsDAL.InsertNews(news)) return RedirectToPage("../News");
+            else info = "Currently, the server cannot fulfill the request.";
+        }
+        dangerBox = true;
+        return Page();
+    }
+}
