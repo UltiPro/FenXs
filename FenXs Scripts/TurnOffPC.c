1a0183a2eecb82d1e5c7631f81d350633af8838f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2022-08-10 18:58:13 +0200

    Update

---
+++ FenXs Scripts/TurnOffPC.c
@@ -0,0 +1,22 @@
+#include "unistd.h"
+#include "stdio.h"
+#include "windows.h"
+
+int main()
+{
+    for(int i=6;i!=0;i--)
+    {
+        printf("The system will shut down after %d0s.\n",i);
+        if(i!=1) sleep(10);
+    }
+    
+    for(int i=9;i!=0;i--)
+    {
+        printf("The system will shut down after %ds.\n",i);
+        sleep(1);
+    }
+
+    system("shutdown -s -t 1");
+
+    return 0;
+}
