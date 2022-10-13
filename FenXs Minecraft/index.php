8f5393a75e92f90f3d4925cff548d4af8031c654
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2022-10-13 11:08:28 +0200

    Update

--- FenXs Minecraft/index.php
+++ FenXs Minecraft/index.php
@@ -10,93 +10,14 @@
 </head>
 
 <body>
-<a href="https://FenXs.ddns.net/">
+<!--<a href="https://FenXs.ddns.net/">
 	<header>
 		Back To Main Page
 	</header>
-</a>
+</a>-->
 
 
-	<article>
-		<div class="lewy">
-			<header>
-				<div>
-					SERWERY MINECRAFT
-				</div>
-			</header>
-			<br />
 
-			<br /><br />
-			<section>
-				<fieldset class="fieldset-last">
-					<legend><b>Minecraft 1.15 - Ulti World</b></legend>
-					<ul>
-						<li>Minecraft Version: <b>1.15</b></li>
-						<li>Typ Serwera: <b>Survival</b></li>
-						<li>Adres IP: <b>nerdownia.ddns.net</b></li>
-						<li>Administracja: <b>Ulti</b></li>
-						<li>Texture Pack: <b>W paczce</b></li>
-						<li>Status Serwera: <b>
-								l'; else echo '<span style="color:red">'.$status.'</span>'; ?></b></li>
-					</ul>
-					<a href="FTP/Minecraft 1.15 Ulti.rar" class="pobierz">
-						<fieldset class="pobierzstyl">
-							<legend>POBIERZ</legend>
-						</fieldset>
-					</a>
-					<br />
-					if((isset($_SESSION['zalogowany']))&&($_SESSION['zalogowany']==true)&&($_SESSION['premium']=="Tak"))
-					{
-					echo '<div class="uruchamianietekst">Zarządzanie Serverem:</div><br />';
-					if($status=="Offline") echo '<a href="PHP/SERWER/wlaczmine.php" class="wlacz">Włącz</a>';
-					if($status=="Online") echo '<a href="PHP/SERWER/RCON/1.15/index.php" class="wylacz" target="blank">Konsola - Zarządzanie</a>';
-					echo '<div class="clearboth"></div>';
-					};
-				</fieldset>
-			</section>
-		</div>
-	</article>
-	<aside>
-		<article>
-			<div class="prawy">
-				<header>
-					<section>
-						<div class="info">
-							<fieldset class="infofieldset">
-								<legend><b>Informacje o grze</b></legend>
-								<div class="infotext">
-									> Gra wymaga wsparcia oprogramowania Java w wersji 64 bitowej.<br />
-									Do pobrania: <a href="https://www.java.com/pl/download/" target="blank" class="odsylacze">>TUTAJ<< /a><br />
-											<hr />
-											> Orginalną wersję gry można zakupić na stronie: <a href="https://www.minecraft.net/pl-pl/" target="blank" class="odsylacze">>MINECRAFT.NET<< /a><br />
-													<hr />
-													> Servery posiadają własną Mape 3D świata!!!<br />
-													(Która jest włączona jedynie przy włączonym serverze gry.)<br />
-													(<span style="color:red;">Guzik do mapy pokaże się nad POBIERZ gdy serwer będzie w trybie Online</span>.)<br />
-													<hr />
-													> W razie jakich kolwiek problemów porszę zgłaszać wszystko w zakładce Kontakt.<br />
-								</div>
-							</fieldset>
-						</div>
-					</section>
-				</header>
-			</div>
-		</article>
-	</aside>
-	</div>
-	</main>
-	<footer>
-		<div class="menudulopacity9"></div>
-		<div class="menudulopacity8"></div>
-		<div class="menudulopacity7"></div>
-		<div class="menudulopacity6"></div>
-		<div class="menudulopacity5"></div>
-		<div class="menudulopacity4"></div>
-		<div class="menudulopacity3"></div>
-		<div class="menudulopacity2"></div>
-		<div class="menudulopacity1"></div>
-	</footer>
-	-->
 </body>
 <script type="text/javascript" src="js/Page-Slider.js"></script>
 
